c1ca1e50cbec131581da53e25a850985b7304443
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-07-11 16:12:32 +0200

    work done on html, beginning js

---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+const difficultySelectorElement = document.getElementById("difficulty_selector");
+const minefieldCreatorElement = document.getElementById("minefield_creator");
+const minefieldElement = document.getElementById("minefield");
