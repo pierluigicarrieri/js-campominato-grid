16474c9356c262beb39ad2a8c9c7fc53df803ae9
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-07-11 16:42:52 +0200

    built function that creates cells

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,50 @@
+"use stict";
+
 const difficultySelectorElement = document.getElementById("difficulty_selector");
 const minefieldCreatorElement = document.getElementById("minefield_creator");
 const minefieldElement = document.getElementById("minefield");
+
+minefieldCreatorElement.addEventListener("click", clickMinefieldCreator);
+
+
+
+function clickMinefieldCreator() {
+
+    const difficulty = parseInt(difficultySelectorElement.value);
+
+    console.log(difficulty);
+
+}
+
+/**
+ * Creates a single minefield cell
+ * @param {string} cellContent Content of the created cell
+ * @returns {HTMLDivElement} Minefield cell
+ */
+
+function cellCreator (cellContent) {
+
+    const cell = document.createElement("div");
+
+    cell.classList.add("cell");
+
+    cell.innerHTML = cellContent;
+
+    return cell;
+
+}
+
+/**
+ * Creates minefiled
+ * @param {number} difficultyArgument Number of cells to put in minefield
+ */
+
+function minefieldCreator (difficultyArgument) {
+
+    for (let i = 0; i < difficultyArgument; i++) {
+
+        cellCreator(i);
+
+    }
+
+}
